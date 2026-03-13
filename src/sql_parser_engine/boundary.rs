7914908819c7ed14c0130fa1af3031646ea511f6
letspurify-ux <letspurify@gmail.com>
2026-03-13 21:23:53 +0900

    fix: split MLE module/signature call specs after quoted LANGUAGE target

--- src/sql_parser_engine/boundary.rs
+++ src/sql_parser_engine/boundary.rs
@@ -353,6 +353,7 @@ impl RoutineFrame {
             if matches!(
                 self.external_clause_state,
                 ExternalClauseState::SawMleKeyword
+                    | ExternalClauseState::SawImplicitQuotedLanguageTarget
                     | ExternalClauseState::SawImplicitLanguageTarget
                     | ExternalClauseState::Confirmed
             ) {
