6935513210c32f80c6bab09c34b211671cadbe99
letspurify-ux <letspurify@gmail.com>
2026-03-13 19:31:55 +0900

    fix: handle additional external language targets in parser splitting

--- src/sql_parser_engine/engine.rs
+++ src/sql_parser_engine/engine.rs
@@ -256,6 +256,7 @@ impl SqlParserEngine {
                 let should_split_before_new_statement = this.state.should_split_before_new_statement_head()
                     && sql_text::is_statement_head_keyword(candidate_upper)
                     && !sql_text::is_external_language_clause_keyword(candidate_upper)
+                    && !is_external_language_target(candidate_upper)
                     && candidate_upper != "BEGIN";
                 let should_split_pending_top_level = this.state.pending_implicit_external_top_level_split
                     && candidate_upper != "BEGIN"
